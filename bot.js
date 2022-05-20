4038cd427ee6aa5148e5819bb324bdab0805372b
mpereah <martin.perea.hernandez@gmail.com>
2022-05-20 15:16:20 -0500

    solo se agrega mas lineas de codigo

--- bot.js
+++ bot.js
@@ -1,11 +1,22 @@
 const { Telegraf } = require("telegraf");
 require('dotenv').config()
 
+
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
 
 bot.start((ctx) =>{
-    ctx.reply('Hola ' + ctx.from.first_name + ' ' + "te muestro una lista de cosas que podemos hacer juntos")
+    console.log(ctx)
+    ctx.reply('Hola ' + ctx.from.first_name + ' ' + "esta es la lista de comandos que puedes utilizar:",
+    "/start" )
 })
+
+bot.command('tok', (ctx) => ctx.reply(ctx.tg.options.apiRoot))
+
+bot.hears('Hola',(ctx) => ctx.reply('Hola ' + ctx.from.first_name))
+
+bot.on('sticker', (ctx) => ctx.reply('👍'))
+
+bot.command('hipster', Telegraf.reply('λ'))
 
 bot.launch()
